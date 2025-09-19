cc51d7682a0ae88cf02c205c09c8c7b404b9a7c6
bunchiochi <ochiojie@gmail.com>
2025-09-19 09:40:44 +0300

    working version

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,11 @@
-use std::io;
+use std::cmp::Ordering;
 use rand::Rng;
+use std::io;
 
 fn main() {
     println!("Guessing Game");
 
-    let secret_number = rand::thread_rng().gen_range(1..=100);
+    let secret_number= rand::thread_rng().gen_range(1..=100);
 
     println!("The secret number is {secret_number}");
 
@@ -16,5 +17,14 @@ fn main() {
         .read_line(&mut guess)
         .expect("Failure to read line");
 
+        let mut guess: u32 = guess.trim().parse().expect("Please type a number");
+
     println!("You have guessed {guess}");
+
+    match guess.cmp(&secret_number) {
+        Ordering::Less => println!("Too small!"),
+        Ordering::Greater => println!("Too big!"),
+        Ordering::Equal => println!("You win!"),
+    }
+
 }
